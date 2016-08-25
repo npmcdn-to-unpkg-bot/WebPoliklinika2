c2ea8e60f491cf6038a8a0aeac23a89e7fe25e08
jumirat <georgemirat3@yahoo.com>
2016-08-25 16:09:13 +0200

    gulpfile.js added

---
+++ src/WebPoliklinika2/gulpfile.js
@@ -0,0 +1,46 @@
+﻿// gulpfile.js
+
+var ts = require('gulp-typescript');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+var destPath = './wwwroot/libs/';
+
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+gulp.task("scriptsNStyles", () => {
+    gulp.src([
+            'core-js/client/**',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/**',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'jquery/dist/jquery.*js',
+            'bootstrap/dist/js/bootstrap.*js',
+    ], {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest("./wwwroot/libs"));
+});
+
+var tsProject = ts.createProject('tsconfig.json');
+gulp.task('ts', function (done) {
+    //var tsResult = tsProject.src()
+    var tsResult = gulp.src([
+            "app/*.ts"
+    ])
+        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+    return tsResult.js.pipe(gulp.dest('./wwwroot/app'));
+});
+
+gulp.task('watch', ['watch.ts']);
+
+gulp.task('watch.ts', ['ts'], function () {
+    return gulp.watch('app/*.ts', ['ts']);
+});
+
+gulp.task('default', ['scriptsNStyles', 'watch']);
